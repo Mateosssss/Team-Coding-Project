830d917dea9c0d4276336cc345f1dc94ba730bb5
Andrii Bryliant <315612@o365.stud.umk.pl>
2026-01-15 14:42:06 +0100

    Created login screen prototype

--- mobile/composeApp/build.gradle.kts
+++ mobile/composeApp/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.material.icons.extended)
 
             implementation(libs.jetbrains.compose.navigation)
             implementation(libs.kotlinx.serialization.json)
---
+++ mobile/composeApp/src/commonMain/kotlin/com/rackncode/cloudwire/presentation/login/LoginViewModel.kt
@@ -0,0 +1,6 @@
+package com.rackncode.cloudwire.presentation.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+}
